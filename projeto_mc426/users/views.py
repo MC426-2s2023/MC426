54f16d1d5d3b7083463ae6e80ddb64a54c8dcdce
Giovanni Gama <gamagiovanni10@gmail.com>
2023-09-27 18:39:52 -0300

    Adiciona funcionalidade de login, junto de alguns Quality of Life

--- projeto_mc426/users/views.py
+++ projeto_mc426/users/views.py
@@ -1,22 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import CreateUserForm
 
-def register(request):
+def registerPage(request):
     form = CreateUserForm()
 
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Conta foi criada para ' + user)
+            return redirect('login')
 
     context = {'form':form}
     return render(request, 'users/register.html', context)
 
-def login(request):
-    if request.method == "GET":
-        return render(request, 'users/login.html')
-    else:
-        return
-        
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Usuário ou senha está incorreto.')
+            return render(request, 'users/login.html')
+
+    return render(request, 'users/login.html')
+    
+def homePage(request):
+    return render(request, 'users/home.html')        
