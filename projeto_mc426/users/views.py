52b85ae1ecefe588727a743b3f80d613a9a3854f
Giovanni Gama <gamagiovanni10@gmail.com>
2023-09-27 17:54:01 -0300

    Adiciona funcionalidade de criação (register) de um novo usuário

--- projeto_mc426/users/views.py
+++ projeto_mc426/users/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import CreateUserForm
 
 def register(request):
-    return render(request, 'users/register.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
 
 def login(request):
     if request.method == "GET":
