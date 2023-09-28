274cdf3a235ae95cd498e1e1c93510988528f1a9
Bruno Sussel Mendes Cunha Bastos <b246992@dac.unicamp.br>
2023-09-28 18:43:58 -0300

    Adicionado modelo de feedback.
    Note que talvez seja legal relacionar um usuário a um
    feedback, mas isso eu deixo pra depois.

--- projeto_mc426/feedback/models.py
+++ projeto_mc426/feedback/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    email = models.EmailField("E-Mail")
+    title = models.CharField("Título", max_length=80)
+    description = models.TextField("Descrição")
+    date = models.DateTimeField("Data de publicação")
