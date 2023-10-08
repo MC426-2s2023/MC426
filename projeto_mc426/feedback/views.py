789a46e7bf6b5646d073a81daf8e18a4bf43ba11
Bruno Sussel Mendes Cunha Bastos <b246992@dac.unicamp.br>
2023-10-08 18:25:24 -0300

    Adicionado verificação de login para acessar feedback.

--- projeto_mc426/feedback/views.py
+++ projeto_mc426/feedback/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from .forms import FeedbackForm
 
 # Create your views here.
+@login_required(login_url='/auth/login')
 def index(request):
     if (request.method == 'POST'):
         form = FeedbackForm(request.POST)
