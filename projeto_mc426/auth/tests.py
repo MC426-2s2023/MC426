bb8c38775e0e07a80e510cb8a02cf5dafc6734f6
joaovsoliveira <jvsolivert@DESKTOP-523M2F3>
2023-10-10 14:49:25 -0300

    adição de testes para auth

--- projeto_mc426/auth/tests.py
+++ projeto_mc426/auth/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from auth import forms
 
-# Create your tests here.
+class UserCreationTest(TestCase): #testa a criação de usuário
+    def testCreateUser(self):
+        forms.User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+        self.assertEqual(forms.User.objects.count(), 1)
+
+class UserAuthenticationTest(TestCase): #testa autenticação
+    def setUp(self):
+        self.user = forms.User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+
+    def test_authenticate_valid_user(self):
+        logged_in = self.client.login(username='testuser', password='testpassword')
+        self.assertTrue(logged_in)
+
+    def test_authenticate_invalid_user(self):
+        logged_in = self.client.login(username='testuser', password='wrongpassword')
+        self.assertFalse(logged_in)
