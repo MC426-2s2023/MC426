1e9e8eedd2190114484822c049b64f70aa9c6743
Bruno Sussel Mendes Cunha Bastos <b246992@dac.unicamp.br>
2023-11-04 18:29:21 -0300

    Adicionado caso de test para usuário não logado.
    Um usuário não logad não pode acessar a página de feedback.

--- projeto_mc426/feedback/tests.py
+++ projeto_mc426/feedback/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class FeedbackTests(TestCase):
+    def test_cannot_access_feedback_site_without_authentication(self):
+        """
+        O usuário deve estar logado para acessar site de feedback.
+        """
+        response = self.client.get(reverse("feedback:feedback"))
+        self.assertEqual(response.status_code, 302) 
+        # 302: redirecionamento temporário
+        self.assertURLEqual(
+            response.url,
+            reverse("login")[:-1] + "?next=" + reverse("feedback:feedback")
+        )
