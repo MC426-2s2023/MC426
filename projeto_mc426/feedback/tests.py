1eb3d6ec7ad765f88794e27bc9e4804c4bd301b6
victorhonoriops <victor_honorio@hotmail.com>
2023-11-05 17:23:51 -0300

    teste de criacao de feedback

--- projeto_mc426/feedback/tests.py
+++ projeto_mc426/feedback/tests.py
@@ -1,10 +1,12 @@
+from time import timezone
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from feedback import forms
 
 # Create your tests here.
 
-class FeedbackTests(TestCase):
+class FeedbackAcessTests(TestCase):
     def test_cannot_access_feedback_site_without_authentication(self):
         """
         O usuário não logado é redirecionado ao site de login.
@@ -27,3 +29,14 @@ class FeedbackTests(TestCase):
         response = self.client.get(reverse("feedback:feedback"), follow=True)
         self.assertEqual(response.status_code, 200)
         # 200: requisição bem sucedida
+
+class FeedbackCreationTest(TestCase):
+    def testCreateFeedback(self):
+        """
+        Verifica se o usuário consegue criar um feedback.
+        """
+        User = get_user_model()
+        user = User.objects.create_user("nome_usuario", "email@a.com", "senha")  
+        forms.Feedback.objects.Feedback(user = user, pub_date = timezone.now(), answer = "", 
+                                        title = "titulo_teste", description = "teste_feedback",)
+        self.assertEqual(forms.Feedback.objects.count(), 1)
