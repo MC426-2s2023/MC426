02ad0f4f24d4647f3b47672b925f335df5cebe44
Bruno Sussel Mendes Cunha Bastos <b246992@dac.unicamp.br>
2023-11-04 22:13:22 -0300

    Criado caso de teste para usuário logado conseguir entrar no site de feedback.

--- projeto_mc426/feedback/tests.py
+++ projeto_mc426/feedback/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 
 class FeedbackTests(TestCase):
     def test_cannot_access_feedback_site_without_authentication(self):
         """
-        O usuário deve estar logado para acessar site de feedback.
+        O usuário não logado é redirecionado ao site de login.
         """
         response = self.client.get(reverse("feedback:feedback"))
         self.assertEqual(response.status_code, 302) 
@@ -15,3 +16,14 @@ class FeedbackTests(TestCase):
             response.url,
             reverse("login")[:-1] + "?next=" + reverse("feedback:feedback")
         )
+   
+    def test_can_access_feedback_site_while_authenticaded(self):
+        """
+        O usuário logado consegue acessar site de feedback diretamente.
+        """
+        User = get_user_model()
+        user = User.objects.create_user("nome_usuario", "email@a.com", "senha")
+        self.client.login(username="nome_usuario", password="senha")
+        response = self.client.get(reverse("feedback:feedback"), follow=True)
+        self.assertEqual(response.status_code, 200)
+        # 200: requisição bem sucedida
