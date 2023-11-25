1f81bf6a928c301b38f830c97b34bf4e8fcfba2d
Giovanni Gama <gamagiovanni10@gmail.com>
2023-11-25 11:22:17 -0300

    Adiciona lógica que entrega os dados do DB para serem tratados pelo js

--- projeto_mc426/home/views.py
+++ projeto_mc426/home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout
-
+from django.core import serializers
+from .models import registro_de_ocorrencia
 # Create your views here.
 
 def homePage(request):
-    return render(request, 'home.html')
+    data = registro_de_ocorrencia.objects.all()
+    context = {
+        "data": data
+    }
+    return render(request, 'home.html', context)
     #if request.user.is_authenticated:
     #    return render(request, 'home.html')
     #else:
