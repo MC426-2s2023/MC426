9177f104eaa64652040cc7e8880a47e9bd29b295
Bruno Sussel Mendes Cunha Bastos <b246992@dac.unicamp.br>
2023-12-02 11:03:02 -0300

    Adicionado re-redirecionamento do login para páginas bloqueadas.
    A tentativa de acessar uma página bloqueada redireciona à página de
    login.  Agora, a página de login, ao ser preenchida, redireciona de
    volta à página bloqueada.

--- projeto_mc426/auth/views.py
+++ projeto_mc426/auth/views.py
@@ -29,7 +29,7 @@ def loginPage(request):
 
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect(request.POST.get('next', 'home'))
         else:
             messages.info(request, 'Usuário ou senha está incorreto.')
             return render(request, 'login.html')
