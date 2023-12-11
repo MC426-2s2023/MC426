3f7e52e447df3ddc80baae9879889b1e8ee8df6d
paulovrn <p247309@dac.unicamp.br>
2023-12-10 23:09:14 -0300

    adicionado logica de busca no banco de dados de ocorrencias em um certo raio

--- projeto_mc426/home/views.py
+++ projeto_mc426/home/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import logout
 from django.core import serializers
 from .models import registro_de_ocorrencia
 from webpush import send_user_notification
+from geopy import distance 
 # Create your views here.
 
 def homePage(request):
@@ -11,10 +12,36 @@ def homePage(request):
     context = {
         "data": data
     }
-    msg = {"head": "Welcome!", "body": "Hello World"}
-    send_user_notification(user=request.user, payload=msg, ttl=1000)
+
     return render(request, 'home.html', context)
+
     #if request.user.is_authenticated:
     #    return render(request, 'home.html')
     #else:
     #    return redirect('../auth/login/')
+
+
+userPosition = None
+
+# executar periodicamente
+def getUserPosition():
+    
+    # if userPosition changed  
+    # warnUser()
+    pass
+
+def getPointsDist(ptA, ptB):
+    return distance.distance(ptA[:2], ptB[:2]).m
+
+def crimeRegisterProximity(lat, lng, radius):
+    ptA = [lat, lng]
+    regs = registro_de_ocorrencia.objects.all()
+
+    crime_regs = []
+    for reg in regs:
+        dist = getPointsDist(ptA, [reg.rdo_lat, reg.rdo_lng])
+        if dist <= radius:
+            crime_regs.append((reg.id, dist))
+
+    return crime_regs.sort(key= lambda vector: vector[1])
+
