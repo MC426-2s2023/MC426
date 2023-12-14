cd252327b4939eb66b8dcacab174658cdb7e55fa
paulovrn <p247309@dac.unicamp.br>
2023-12-14 00:13:04 -0300

    ajustes na home, raiz redireciona pra home

--- projeto_mc426/projeto_mc426/urls.py
+++ projeto_mc426/projeto_mc426/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render, redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth.urls')),
     path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('crime_register/', include('register.urls')),
 ]
