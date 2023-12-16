410dd775bbbc51f14307308adc4b940334b4a0d7
paulovrn <p247309@dac.unicamp.br>
2023-12-15 23:01:36 -0300

    corrige testes de coordenadas

--- projeto_mc426/auth/models.py
+++ projeto_mc426/auth/models.py
@@ -2,30 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.test.testcases import ValidationError
 from home.warning import Warning
 from django.core.validators import MaxValueValidator, MinValueValidator
+from decimal import Decimal
 
 # OneToOne link para usuario padrao do Django
 class LocationProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True, related_name = "location_profile")
-    user_lat = models.DecimalField("latitude", max_digits=10, decimal_places=6,
-        validators=[
-            MaxValueValidator(90),
-            MinValueValidator(-90)
-        ])
-    user_lng = models.DecimalField("longitude", max_digits=10, decimal_places=6,
-        validators=[
-            MaxValueValidator(180),
-            MinValueValidator(-180)
-        ])
+    user_lat = models.DecimalField("latitude", max_digits=10, decimal_places=6)
+    user_lng = models.DecimalField("longitude", max_digits=10, decimal_places=6)
     subs = [Warning()]
 
     def subscribe(self, newSub):
         self.subs.append(newSub)
 
     def setLatLng(self,lat, lng):
-        self.lat = lat
-        self.lng = lng
+        if self.is_position_valid(lat,lng):
+            self.lat = lat
+            self.lng = lng
+        else:
+            raise ValidationError("Invalid Position")
+
+
+    def is_position_valid(self, lat, lng):
+        if (lat < -90 or lat>90) or (lng < -180 or lng > 180):
+            return False
+        return True
 
     def updateUserLocation(self, request, lat, lng):
         if self.user_lat == lat and self.user_lng == lng: # posicao nao foi alterada
